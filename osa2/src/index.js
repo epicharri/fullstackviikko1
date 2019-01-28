42e50334ab9ef1ceeb06c3b4a813c0bc3f5118ae
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-01-28 15:06:56 +0200

    Osa 2 aloitus, kopiotu osa1

---
+++ osa2/src/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14
+                }
+            ]
+    }
+
+    return (
+        <>
+            <Header course={course} />
+            
+            <Content course={course} />
+            <Total course={course} />    
+        </>
+    )
+}
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course.name}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+
+    return (
+        <div>
+            <Part part = {props.course.parts[0]} />
+            <Part part = {props.course.parts[1]} />
+            <Part part = {props.course.parts[2]} />
+        </div>
+    )
+}
+
+const Part = (props) => {
+
+    return (
+        <div>
+            <p>
+                {props.part.name} {props.part.exercises}
+            </p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    
+    console.log('props', props)
+    console.log('props.course', props.course)
+    console.log('props.course.parts', props.course.parts)
+    console.log('props.course.parts[0]', props.course.parts[0])
+
+    return (
+        <div>
+            <p>
+                yhteensä {
+                    props.course.parts[0].exercises +
+                    props.course.parts[1].exercises +
+                    props.course.parts[2].exercises} tehtävää
+            </p>
+        </div>
+
+    )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
