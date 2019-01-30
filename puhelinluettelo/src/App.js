ef301c9334e274c5552ad16dc690d78533860495
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-01-30 17:47:22 +0200

    2.8

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from "react"
 import "./App.css"
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Harri Kähkönen", id: 1 }])
+  const [persons, setPersons] = useState([{ name: "Harri Kähkönen", number:"044-1234567", id: 1 }])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const handleNewName = event => {
     event.preventDefault()
     setNewName(event.target.value)
+  }
+
+  const handleNewNumber = event => {
+    event.preventDefault()
+    setNewNumber(event.target.value)
   }
 
   const addPerson = event => {
@@ -16,6 +22,7 @@ const App = () => {
       setPersons(
         persons.concat({
           name: newName,
+          number: newNumber,
           id: persons.length + 1
         })
       )
@@ -29,12 +36,13 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           nimi: <input value={newName} onChange={handleNewName} />
+          numero: <input value={newNumber} onChange={handleNewNumber} />
           <button type="submit">Lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
       {persons.map(person => {
-        return <div key={person.id}>{person.name}</div>
+        return <div key={person.id}>{person.name} {person.number}</div>
       })}
     </div>
   )
