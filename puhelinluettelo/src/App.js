84dfc7102bc3d5fe666b1786283f43f3ce9a17eb
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-02-02 21:42:09 +0200

    Tehtävät 2.20 asti puhelinluettelon osalta

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,31 +1,32 @@
-import React, { useState } from "react"
-import "./App.css"
+import React, {
+  useState,
+  useEffect
+} from "react"
+//import axios from 'axios'
+import personService from "./services/persons"
 
 const Input = props => {
-  const { text, newSomething, handleNewSomething } = props
+  const {
+    text,
+    newSomething,
+    handleNewSomething
+  } = props
   return (
     <div>
-      {text} <input value={newSomething} onChange={handleNewSomething} />
+      {text}{" "}
+      <input
+        value={newSomething}
+        onChange={handleNewSomething}
+      />
     </div>
   )
 }
 
-const Listing = props => {
-  const { persons, newSearch } = props
-  return persons
-    .filter(person =>
-      person.name.toLocaleLowerCase().includes(newSearch.toLocaleLowerCase())
-    )
-    .map(person => {
-      return (
-        <div key={person.id}>
-          {person.name} {person.number}
-        </div>
-      )
-    })
-}
-
 const ReadPerson = props => {
+  console.log(
+    "props readpersonissa on ",
+    props
+  )
   const {
     addPerson,
     newName,
@@ -39,28 +40,130 @@ const ReadPerson = props => {
         <Input
           text={"Nimi: "}
           newSomething={newName}
-          handleNewSomething={handleNewName}
+          handleNewSomething={
+            handleNewName
+          }
         />
         <Input
           text={"Numero: "}
           newSomething={newNumber}
-          handleNewSomething={handleNewNumber}
+          handleNewSomething={
+            handleNewNumber
+          }
         />
-        <button type="submit">Lisää</button>
+        <button type="submit">
+          Lisää
+        </button>
       </div>
     </form>
   )
 }
 
+const Listing = props => {
+  const {
+    persons,
+    newSearch,
+    deletePerson
+  } = props
+
+  return persons
+    .filter(person =>
+      person.name
+        .toLocaleLowerCase()
+        .includes(
+          newSearch.toLocaleLowerCase()
+        )
+    )
+    .map(person => {
+      return (
+        <div key={person.id}>
+          {person.name} {person.number}
+          <button
+            value={person.id}
+            onClick={deletePerson}
+          >
+            Poista
+          </button>
+        </div>
+      )
+    })
+}
+
+const Notification = ({ message, style }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={style}>
+      {message}
+    </div>
+  )
+}
+
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Harri Kähkönen", number: "044-1234567", id: 1 },
-    { name: "Frozen Elsa", number: "111-111111", id: 2 },
-    { name: "Lumi Ukko", number: "222-22222", id: 3 }
-  ])
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [newSearch, setNewSearch] = useState("")
+  const [
+    persons,
+    setPersons
+  ] = useState([])
+
+
+
+
+  const hook = () => {
+    console.log("effect")
+    personService
+      .getAll()
+      .then(returnedPerson => {
+        console.log("promise fulfilled")
+        setPersons(returnedPerson)
+      })
+  }
+  useEffect(hook, [])
+  console.log(
+    "render",
+    persons.length,
+    "persons"
+  )
+
+  const [
+    newName,
+    setNewName
+  ] = useState("")
+  const [
+    newNumber,
+    setNewNumber
+  ] = useState("")
+  const [
+    newSearch,
+    setNewSearch
+  ] = useState("")
+
+  const [
+    errorMessage, 
+    setErrorMessage
+  ] = useState('')
+
+  const [
+    errorStyle,
+    setErrorStyle
+  ] = useState('normal')
+
+  const handleErrorMessage = props => {
+    console.log(props)
+  
+    setErrorMessage(props)
+    setTimeout(() => {
+      setErrorMessage(null)
+      setErrorStyle('normal')
+    }, 
+      3000
+    )
+  }
+
+
+
 
   const handleNewName = event => {
     event.preventDefault()
@@ -74,44 +177,149 @@ const App = () => {
 
   const handleNewSearch = event => {
     event.preventDefault()
+    console.log(
+      "event.target.value: ",
+      event.target.value
+    )
+    console.log(
+      "newSearch: ",
+      newSearch
+    )
     setNewSearch(event.target.value)
+    console.log(
+      "newSearch: ",
+      newSearch
+    )
+  }
+
+  const deletePerson = event => {
+    event.preventDefault()
+    const id = "" + event.target.value
+    const person = persons.find(
+      person => {
+        return "" + person.id === id
+      }
+    )
+
+    const saaPoistaa = window.confirm(
+      `Saako henkilön ${
+        person.name
+      } poistaa?`
+    )
+    if (saaPoistaa) {
+      personService
+        .deletePersonById(id)
+        .then(() => {
+          setErrorStyle('success')
+          handleErrorMessage('Henkilön poisto onnistui!')
+          return hook()
+          }
+        )
+        .catch(() => {
+          setErrorStyle('error')
+          handleErrorMessage('Henkilö oli jo poistettu!')
+          return hook()
+        }
+        )
+    }
   }
 
   const addPerson = event => {
     event.preventDefault()
-    if (!persons.map(person => person.name).includes(newName)) {
-      setPersons(
-        persons.concat({
+    if (newName === '') {
+      setErrorStyle('error')
+      handleErrorMessage('Et ole vielä syöttänyt nimeä.')
+      
+    } else {
+      if (
+        !persons
+          .map(person => person.name)
+          .includes(newName)
+      ) {
+        const person = {
           name: newName,
-          number: newNumber,
-          id: persons.length + 1
-        })
-      )
-      setNewName("")
-      setNewNumber("")
-      setNewSearch("")
-    } else window.alert(`${newName} on jo luettelossa!`)
+          number: newNumber
+        }
+        personService
+          .create(person)
+          .then(returnedPerson => {
+
+            setPersons(
+              persons.concat(
+                returnedPerson
+              )
+            )
+            setErrorStyle('success')
+            handleErrorMessage('Lisääminen onnistui!')
+
+            setNewName("")
+            setNewNumber("")
+            setNewSearch("")
+          })
+      } else {
+        const korvataanNumero = window.confirm(
+          `${newName} on jo luettelossa. Korvataanko vanha numero uudella?`
+        )
+        if (korvataanNumero) {
+          const uusiPerson = {
+            name: newName,
+            number: newNumber
+          }
+          const vanhaPerson = persons.find(
+            person =>
+              person.name === newName
+          )
+          personService
+            .update(
+              vanhaPerson.id,
+              uusiPerson
+            )
+            .then(() => {
+              setErrorStyle('success')
+              handleErrorMessage(`Päivittäminen onnistui!`)
+              return hook()
+            })
+            .catch(() => {
+              setErrorStyle('error')
+              handleErrorMessage('Henkilö oli jo poistettu, ei voi päivittää!')
+              return hook()
+            }
+            )
+        }
+      }
+    }
   }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+
+      <Notification message={errorMessage} style={errorStyle} />
+
       <Input
         text={"Hae: "}
         newSomething={newSearch}
-        handleNewSomething={handleNewSearch}
+        handleNewSomething={
+          handleNewSearch
+        }
       />
       <ReadPerson
         addPerson={addPerson}
         newName={newName}
         handleNewName={handleNewName}
         newNumber={newNumber}
-        handleNewNumber={handleNewNumber}
+        handleNewNumber={
+          handleNewNumber
+        }
       />
 
       <h2>Numerot</h2>
 
-      <Listing persons={persons} newSearch={newSearch} />
+      <Listing
+        persons={persons}
+        newSearch={newSearch}
+        deletePerson={deletePerson}
+      />
     </div>
   )
 }
---
+++ puhelinluettelo/src/services/persons.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+// import { request } from 'https';
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const deletePersonById = id => {
+  try {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+  } catch(error) {
+    console.log('Tää on persons.js:ssä errorina: ', error)
+  }
+
+  }
+
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update,
+  deletePersonById: deletePersonById
+}
